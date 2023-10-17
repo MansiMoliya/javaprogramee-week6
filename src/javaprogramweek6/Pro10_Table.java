1b579bf328676f05a0438751d47601a6cfc4b904
MansiMoliya <146835821+MansiMoliya@users.noreply.github.com>
2023-10-17 14:01:06 +0100

    Update Pro10_Table.java

--- src/javaprogramweek6/Pro10_Table.java
+++ src/javaprogramweek6/Pro10_Table.java
@@ -3,17 +3,31 @@ package javaprogramweek6;
 import java.util.Scanner;
 
 public class Pro10_Table {
-    public static void table(int a){
-        for (int i=1;i<=10;i++){
-            System.out.println(a+"*"+i+"="+a*i);
-        }
+    public static void table(int num, int i) {
+        //  for (int i=1;i<=10;i++){
+        //    System.out.println(a+"*"+i+"="+a*i)
+
+        System.out.println(num + "*" + i + "=" + num * i);
+
+    }
+    public static void print(int num) {
+        table(num,1);
+        table(num,2);
+        table(num,3);
+        table(num,4);
+        table(num,5);
+        table(num,6);
+        table(num,7);
+        table(num,8);
+        table(num,9);
+        table(num,10);
     }
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value:");
-        int a=sc.nextInt();
-        table(a);
+        int a = sc.nextInt();
+        print(a);
         sc.close();
 
     }
